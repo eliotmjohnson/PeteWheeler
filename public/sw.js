34560791a8027f30329376796d21d280d994fd15
Eliot Johnson <eliotmjohnson@gmail.com>
2026-07-31 22:39:18 -0500

    Finally

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'petewheeler-v5';
+const CACHE_NAME = 'petewheeler-v6';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icons/petewheeler-icon.svg'];
 
 self.addEventListener('install', (event) => {
